ed187e821d39e15e72f0bf8263a0e814f524a36f
Gustavo Mota <gustavo.omota@hotmail.com>
2020-07-08 21:00:03 -0300

    Versão inicial do projeto

---
+++ src/test/java/com/guomota/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guomota.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
