9141dd5371d188e7eccf80ae4d226d3554b7217d
Gustavo Mota <gustavo.omota@hotmail.com>
2020-07-09 13:49:54 -0300

    Novas dependencias

--- src/main/java/com/guomota/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/guomota/api/MeuPrimeiroProjetoApplication.java
@@ -1,14 +1,28 @@
 package com.guomota.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
-
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
 		System.out.println("Meu primeiro projeto...");
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("### Quantidade de elementos por página = " + this.qtdPorPagina);
+		};
+		
 	}
 
 }
